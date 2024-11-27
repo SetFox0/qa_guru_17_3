fbb0f391effc35bf814cf4848c22160830cf8982
Set Fox <anarhyfox@gmail.com>
2024-11-27 18:04:36 +0300

    Added 2 tests: successful and failed

--- test_browser_search.py
+++ test_browser_search.py
@@ -6,5 +6,5 @@ def test_successful_search(setting_browser):
     browser.element('[id="search"]').should(have.text('Tests with Selene'))
 
 def test_failed_search(setting_browser):
-    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-    browser.element('[id="footcnt"]').should(have.text('This text cannot exist'))
+    browser.element('[name="q"]').should(be.blank).type('jkeaj2893--=2-3....232dewdwae!uYGujhb').press_enter()
+    browser.element('[id="rcnt"]').should(have.text('No results containing all your search terms were found.'))
